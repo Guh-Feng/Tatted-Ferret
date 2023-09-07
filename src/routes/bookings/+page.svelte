b99a4f3995c1a4a8de899f55556fba5ad287f6f0
Guh-Feng <58538080+Guh-Feng@users.noreply.github.com>
2023-09-07 01:48:34 -0400

    Booking Layout

--- src/routes/bookings/+page.svelte
+++ src/routes/bookings/+page.svelte
@@ -4,3 +4,4 @@
 	$: activeUrl = $page.url.pathname;
 </script>
 
+<img class="absolute left-80 top-0 ml-10 -z-10 max-h-none max-w-none" height="645" width="645" src="/booking/booking.png" alt="contacts-screen"/>
