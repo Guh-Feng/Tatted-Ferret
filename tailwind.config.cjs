8dcf90d230e92d7202760d91b3497699baf0041f
Guh-Feng <58538080+Guh-Feng@users.noreply.github.com>
2024-04-23 23:30:21 -0400

    Adjusted Tailwind to Just In Time Mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 const config = {
+	mode: 'jit', 
+
 	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-  
+
 	plugins: [require('flowbite/plugin')],
   
 	darkMode: 'class',
